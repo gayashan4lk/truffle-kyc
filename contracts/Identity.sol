3bc26495468d721c27abf471d04dacd60b64df17
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-05-30 10:35:20 +0530

    no major changes

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -8,8 +8,8 @@ contract Identity {
     uint32 public signRecordId = 0;
 
     enum UserType {
-        Issuer,
         Owner,
+        Issuer,
         Verifier
     }
 
@@ -53,17 +53,47 @@ contract Identity {
 
     mapping(address => mapping(uint32 => SignRecord)) public SignRecordBook;
 
-    function addUser(address _address, string memory _name, string memory _pass, UserType _type) public returns (uint32) {
-        uint32 _id = userId++;
-        Users[_id] = User(_address, _name, _pass, _type);
-        return _id;
+    function addUser(
+        address _address,
+        string memory _name,
+        string memory _pass,
+        string memory _type
+    ) public returns (uint32) {
+        if (
+            keccak256(abi.encodePacked(_type)) == keccak256("Issuer") ||
+            keccak256(abi.encodePacked(_type)) == keccak256("Owner") ||
+            keccak256(abi.encodePacked(_type)) == keccak256("Verifier")
+        ) {
+            UserType _userType;
+            if (keccak256(abi.encodePacked(_type)) == keccak256("Issuer")) {
+                _userType = UserType.Issuer;
+            } else if (
+                keccak256(abi.encodePacked(_type)) == keccak256("Owner")
+            ) {
+                _userType = UserType.Owner;
+            } else if (
+                keccak256(abi.encodePacked(_type)) == keccak256("Verifier")
+            ) {
+                _userType = UserType.Verifier;
+            }
+            uint32 _id = userId++;
+            Users[_id] = User(_address, _name, _pass, _userType);
+            return _id;
+        }
+        return 0;
     }
 
     function getUser(uint32 _id) public view returns (User memory) {
         return (Users[_id]);
     }
 
-    function addCredentials(uint32 _userId, address _owner, address _issuer, string memory _name, string memory _details) public returns (uint32) {
+    function addCredentials(
+        uint32 _userId,
+        address _owner,
+        address _issuer,
+        string memory _name,
+        string memory _details
+    ) public returns (uint32) {
         if (Users[_userId].userType == UserType.Owner) {
             uint32 _credentialsId = credentialsId++;
             CredentialsList[_owner][_credentialsId] = Credentials(
@@ -79,11 +109,19 @@ contract Identity {
         return 0;
     }
 
-    function getCredentialsByUser(address _address, uint32 _id) public view returns (Credentials memory) {
+    function getCredentialsByOwner(address _address, uint32 _id)
+        public
+        view
+        returns (Credentials memory)
+    {
         return (CredentialsList[_address][_id]);
     }
 
-    function revealCredentials(uint32 _ownerId, uint32 _verifierId, uint32 _credentialsId) public returns (uint32) {
+    function revealCredentials(
+        uint32 _ownerId,
+        uint32 _verifierId,
+        uint32 _credentialsId
+    ) public returns (uint32) {
         if (Users[_ownerId].userType == UserType.Owner) {
             uint32 _id = revealRecordId++;
             RevealRecordBook[Users[_ownerId].userAddress][_id] = RevealRecord(
@@ -98,7 +136,11 @@ contract Identity {
         return 0;
     }
 
-    function requestCredentials(uint32 _verifierId, uint32 _ownerId, uint32 _credentialsId) public returns (uint32) {
+    function requestCredentials(
+        uint32 _verifierId,
+        uint32 _ownerId,
+        uint32 _credentialsId
+    ) public returns (uint32) {
         if (Users[_verifierId].userType == UserType.Verifier) {
             uint32 _id = revealRecordId++;
             RevealRecordBook[Users[_ownerId].userAddress][_id] = RevealRecord(
@@ -113,7 +155,19 @@ contract Identity {
         return 0;
     }
 
-    function signCredentials(uint32 _ownerId, uint32 _issuerId, uint32 _credentialsId) public returns (uint32) {
+    function getRevealRecord(address _ownerAddress, uint32 _id)
+        public
+        view
+        returns (RevealRecord memory)
+    {
+        return (RevealRecordBook[_ownerAddress][_id]);
+    }
+
+    function signCredentials(
+        uint32 _ownerId,
+        uint32 _issuerId,
+        uint32 _credentialsId
+    ) public returns (uint32) {
         if (Users[_issuerId].userType == UserType.Issuer) {
             uint32 _id = signRecordId++;
             SignRecordBook[Users[_ownerId].userAddress][_id] = SignRecord(
@@ -128,7 +182,11 @@ contract Identity {
         return 0;
     }
 
-    function unSignCredentials(uint32 _ownerId, uint32 _issuerId, uint32 _credentialsId) public returns (uint32) {
+    function unSignCredentials(
+        uint32 _ownerId,
+        uint32 _issuerId,
+        uint32 _credentialsId
+    ) public returns (uint32) {
         if (Users[_issuerId].userType == UserType.Issuer) {
             uint32 _id = signRecordId++;
             SignRecordBook[Users[_ownerId].userAddress][_id] = SignRecord(
@@ -141,5 +199,13 @@ contract Identity {
             return (_id);
         }
         return 0;
+    }
+
+    function getSignRecord(address _ownerAddress, uint32 _id)
+        public
+        view
+        returns (SignRecord memory)
+    {
+        return (SignRecordBook[_ownerAddress][_id]);
     }
 }
